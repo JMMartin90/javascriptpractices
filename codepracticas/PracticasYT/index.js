957ed1b4630fb4e8b5cae7c1c6ddfbd3c43fb857
JMMartin90 <44844966+JMMartin90@users.noreply.github.com>
2022-03-24 19:35:30 +0100

    pretier

--- codepracticas/PracticasYT/index.js
+++ codepracticas/PracticasYT/index.js
@@ -1,51 +1,47 @@
-console.log('Hello world'); 
+console.log("Hello world");
 
 //primitives values
 
-let name = 'pepe'; // string literal 
+let name = "pepe"; // string literal
 let age = 30; // number literal
-let isApproved = false; // boolean literal   
+let isApproved = false; // boolean literal
 let fistName = undefined;
 let selectedColor = null;
 
 // reference types
 
 let person = {
- name: 'pepe',
-   age : 30
+  name: "pepe",
+  age: 30,
 };
 
 // Dot notation
-person.name = 'juan';
+person.name = "juan";
 
 // Bracket Notation
-person['name'] = 'maria'
+person["name"] = "maria";
 
 console.log(person);
 
 // array
 
-let selectedColors = ['red', 'blue'];
-selectedColors[2] = 'green'
-console.log(selectedColors)
+let selectedColors = ["red", "blue"];
+selectedColors[2] = "green";
+console.log(selectedColors);
 
 // function
 
 //performing a task
 function greet(name, lastName) {
-    console.log('hello' + ' ' + name + ' ' + lastName)
+  console.log("hello" + " " + name + " " + lastName);
 }
 
-greet('pepe', 'rodrigez');
+greet("pepe", "rodrigez");
 
 // Calculating a value
 
 function square(number) {
-    return number * number;
+  return number * number;
 }
-   
 
-
-console.log(square(2))
-
-
+console.log(square(2));
--- codepracticas/practicaweb/scripts/funciones.js
+++ codepracticas/practicaweb/scripts/funciones.js
@@ -3,31 +3,31 @@
 // declaramos una función con la palabra clave "function", seguida del nombre de la función
 // y entre parentesis los valores (argumentos) que acepta la función.
 function log(valor) {
-    console.log(valor)
+  console.log(valor);
 }
 // llamamos a la función log de la siguiente manera.
-log("hello world")
+log("hello world");
 
 function doble(n) {
-    return n*2
+  return n * 2;
 }
 
-doble(4)
+doble(4);
 
-const dobleDe4 = doble(4)
-log(dobleDe4)
-log(doble(4))
+const dobleDe4 = doble(4);
+log(dobleDe4);
+log(doble(4));
 /* para descubrir un numero como impar o par, 
 la solución mas facil seria aplicarle un 
 resto por 2( %2) con la funcion (if),
 si fuera par daria verdadero y ejecutaria (console.log("par") 
 y si fuera falso se ejecuta (else)*/
 function esPar(n) {
-    const resultado = n%2
-    if (resultado ===0) {
-        console.log("par")
-    } else {
-        console.log("impar")
-    }
-    return !Boolean(resultado) //para finalizar utilizamos la funcion (return) para darnos verdadero o falso dependiendo del resultado.
+  const resultado = n % 2;
+  if (resultado === 0) {
+    console.log("par");
+  } else {
+    console.log("impar");
+  }
+  return !Boolean(resultado); //para finalizar utilizamos la funcion (return) para darnos verdadero o falso dependiendo del resultado.
 }
